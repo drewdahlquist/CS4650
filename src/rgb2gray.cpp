04b811ae59056385078e44fcd6b058e0066a6e55
Drew Dahlquist <gypysmoth@gmail.com>
2021-09-07 20:23:15 -0500

    rgb2gray completed

--- src/rgb2gray.cpp
+++ src/rgb2gray.cpp
@@ -9,24 +9,33 @@ int main(int argc, char** argv )
         return -1;
     }
 
-    cv::Mat img = cv::imread( argv[1], 1 );
+    // read input image & create 0 image for output
+    cv::Mat img = cv::imread(argv[1], 1);
     if ( !img.data )
     {
         std::cout << "could not read image: " << argv[1] << std::endl;
         return -1;
     }
+    cv::Mat gray = cv::Mat::zeros(img.size(), 0);
     
-    uint8_t rows = img.rows;
-    uint8_t cols = img.cols;
-    for(uint8_t r = 0; r < rows; ++r) {
-        for(uint8_t c = 0; c < cols; ++c) {
-            img.at<uchar>(r, c) = 0;
+    // do our computation for rbg (really it's bgr) -> grayscale
+    int rows = img.rows;
+    int cols = img.cols;
+    int channels = img.channels();
+    for(int i = 0; i < rows; ++i) {
+        for(int j = 0; j < cols; ++j) {
+            float val = 0.1140*img.at<cv::Vec3b>(i,j)[0] + 0.5871*img.at<cv::Vec3b>(i,j)[1] + 0.2989*img.at<cv::Vec3b>(i,j)[2];
+            if(val < 0) { val = 0; }
+            else if(val > 255) { val = 255; }
+            gray.at<uint8_t>(i,j) = val;
         }
     }
     
-    cv::namedWindow("rgb2gray", cv::WINDOW_AUTOSIZE );
-    cv::imshow("rgb2gray", img);
-    cv::waitKey(0);
+    cv:imwrite(argv[2], gray);
+
+    // cv::namedWindow("rgb2gray", cv::WINDOW_AUTOSIZE );
+    // cv::imshow("rgb2gray", gray);
+    // cv::waitKey(0);
 
     return 0;
 }
