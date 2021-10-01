a126cc231b3bdcc7edddf95b3db71de9d1e2e3d2
Drew Dahlquist <gypysmoth@gmail.com>
2021-09-30 23:29:28 -0500

    need to handle data type issue for cc label & also implement union-find part

--- hw2/src/ccl.cpp
+++ hw2/src/ccl.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 
+#define WHITE 255
+#define BLACK 0
+
 int ccl_find(int parent[], int i)
 {
     if (parent[i] == -1)
@@ -30,53 +33,79 @@ int main(int argc, char** argv )
         std::cout << "could not read image: " << argv[2] << std::endl;
         return -1;
     }
-    cv::Mat labeled = cv::Mat::zeros(img.size(), 0);
+    cv::Mat labeled = cv::Mat::zeros(img.size(), 0); // 2 = CV_16U
+
+    // define size once & for all
+    int rows = img.rows;
+    int cols = img.cols;
+    uint8_t thold = WHITE/4; // also threshold for some stuff
     
-    // invert 0's & 1's
+    // optionally invert 0's & 1's
     if(atoi(argv[1]) == 1) {
         cv::Mat inv = cv::Mat::zeros(img.size(), 0);
-        int rows = img.rows;
-        int cols = img.cols;
         for(int r = 0; r < rows; ++r) {
             for(int c = 0; c < cols; ++c) {
                 // 1 -> 0
-                if(img.at<uint8_t>(r,c) == 255) {
-                    inv.at<uint8_t>(r,c) = 0;
+                if(img.at<uint8_t>(r,c) > thold) {
+                    inv.at<uint8_t>(r,c) = BLACK;
                 }
                 // 0 -> 1
                 else {
-                    inv.at<uint8_t>(r,c) = 255;
+                    inv.at<uint8_t>(r,c) = WHITE;
                 }
             }
         }
         img = inv;
     }
 
+    // ensure image we handle is actually binary {0, 65535}, threshold at mid point
+    cv::Mat bin = cv::Mat::zeros(img.size(), 0);
+    for(int r = 0; r < rows; ++r) {
+        for(int c = 0; c < cols; ++c) {
+            uint8_t val;
+            if(img.at<uint8_t>(r,c) < thold) { val = BLACK; }
+            else { val = WHITE; }
+            bin.at<uint8_t>(r,c) = val;
+        }
+    }
+    img = bin;
+
     int parent [2048];
     
     // 1st scan
-    int rows = img.rows;
-    int cols = img.cols;
     int cc = 0;
     for(int r = 0; r < rows; ++r) {
         for(int c = 0; c < cols; ++c) {
             // if pix 1 & connected
-            if(img.at<uint8_t>(r,c) == 255 && (img.at<uint8_t>(r,c-1) != 0 || img.at<uint8_t>(r-1,c-1) != 0 || img.at<uint8_t>(r-1,c) != 0)) {
-                labeled.at<uint8_t>(r,c) = std::min(std::min(img.at<uint8_t>(r,c-1), img.at<uint8_t>(r-1,c-1)), img.at<uint8_t>(r-1,c)); // propogate min label
+            if(img.at<uint8_t>(r,c) == WHITE && (img.at<uint8_t>(r,c-1) == WHITE || img.at<uint8_t>(r-1,c-1) == WHITE || img.at<uint8_t>(r-1,c) == WHITE)) {
+                // propogate label
+                if(img.at<uint8_t>(r,c-1) == WHITE) {
+                    labeled.at<uint8_t>(r,c) = labeled.at<uint8_t>(r,c-1);
+                }
+                else if(img.at<uint8_t>(r-1,c-1) == WHITE) {
+                    labeled.at<uint8_t>(r,c) = labeled.at<uint8_t>(r-1,c-1);
+                }
+                else {
+                    labeled.at<uint8_t>(r,c) = labeled.at<uint8_t>(r-1,c);
+                }
             }
             // if pix 1 & not connected
-            else if(img.at<uint8_t>(r,c) == 255 && (img.at<uint8_t>(r,c-1) == 0 || img.at<uint8_t>(r-1,c-1) == 0 || img.at<uint8_t>(r-1,c) == 0)) {
+            else if(img.at<uint8_t>(r,c) == WHITE && (img.at<uint8_t>(r,c-1) == BLACK && img.at<uint8_t>(r-1,c-1) == BLACK && img.at<uint8_t>(r-1,c) == BLACK)) {
                 ++cc;
-                labeled.at<uint8_t>(r,c) = cc;
+                // labeled.at<uint8_t>(r,c) = cc;
+                labeled.at<uint8_t>(r,c) = WHITE/2;
             }
             // if 0
-            else if(img.at<uint8_t>(r,c) == 0) {
-                labeled.at<uint8_t>(r,c) = 0;
+            else if(img.at<uint8_t>(r,c) == BLACK) {
+                labeled.at<uint8_t>(r,c) = BLACK;
             }
         }
     }
     
     cv:imwrite(argv[3], img);
+
+    std::cout << "Statistics" << std::endl;
+    std::cout << "Intermediate labels: " << cc << std::endl;
 
     // cv::namedWindow("CCL", cv::WINDOW_AUTOSIZE );
     // cv::imshow("CCL", gray);
