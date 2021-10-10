2c2226160920ac70be89d23269dd518780e37a80
Drew Dahlquist <gypysmoth@gmail.com>
2021-10-10 16:27:06 -0500

    translate function done

--- hw3/src/main.py
+++ hw3/src/main.py
@@ -1,8 +1,16 @@
 import cv2 as cv
+import numpy as np
 import sys
+import random as rand
 
 def translate(img, tx, ty):
-    pass
+    height, width = img.shape[:2]
+    T = np.float32([
+        [1, 0, tx],
+        [0, 1, ty]
+    ])
+    new = cv.warpAffine(img, T, (height, width))
+    return new
 
 def crop(img):
     pass
@@ -27,8 +35,12 @@ def blur(img, filter_size):
 
 
 if __name__ == '__main__':
-    
-    if(len(sys.argv) != 2):
-        print('Usage: python3 main.py input.png')
+
+    if(len(sys.argv) != 3):
+        print('Usage: python3 main.py input.jpg output_dir')
 
     img = cv.imread(str(sys.argv[1]))
+
+    # 5 transformations for each implemented function
+    for i in range(5):
+        cv.imwrite(sys.argv[2]+'/translate_'+str(i)+'.jpg', translate(img, rand.choice([-1,1])*(img.shape[0]/rand.randint(2,8)), rand.choice([-1,1])*(img.shape[1]/rand.randint(2,8))))
